def9007404c0a70e7cee7dd2785f7bd942a8502d
Trevor B <t.brixey0129@gmail.com>
2020-04-30 02:18:39 -0500

    fuck

--- app.js
+++ app.js
@@ -6,28 +6,30 @@ require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-// Connection URL
 const url = process.env.MONGO_URL;
-
-// Database Name
 const dbName = process.env.DB_NAME;
 
-// Use connect method to connect to the server
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
 
-    const db = client.db(dbName, { useUnifiedTopology: true });
+  client.close();
+});
 
-    client.close();
-  }
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST");
+  res.header("Access-Control-Allow-Credentials", "true");
+});
 
 app.get("/", function (req, res) {
   res.send(JSON.stringify({ Hello: "World" }));
+});
+
+app.get("/sites", (req, res) => {
+  console.log("SITES", req.query);
+  res.send({ data: "yo" });
 });
 
 app.listen(port, function () {
