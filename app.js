e0d1f820965801cdd23d66f35982571be0f63f16
Trevor B <t.brixey0129@gmail.com>
2020-04-30 02:23:33 -0500

    remove use stuff

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
   client.close();
 });
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST");
-  res.header("Access-Control-Allow-Credentials", "true");
-});
-
 app.get("/", function (req, res) {
   res.send(JSON.stringify({ Hello: "World" }));
 });
